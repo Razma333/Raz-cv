2c2c6f4ab4e179bb560a33e4c7cb607a1da99bd5
Razma333 <Raz5945@gmail.com>
2017-03-19 21:46:56 +0200

    experince component create, styling ,main html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { CvNavbarComponent } from '../app/cv-navbar/cv-navbar.component';
 import { TechKnowledgeComponent } from '../app/tech-knowledge/tech-knowledge.component';
+import { ExperienceComponent } from '../app/experience/experience.component';
 
 @NgModule({
   imports: [BrowserModule],
   declarations: [
     AppComponent,
     CvNavbarComponent,
-    TechKnowledgeComponent
+    TechKnowledgeComponent,
+    ExperienceComponent
   ],
   bootstrap: [AppComponent]
 })
