86b1d839cf1b46e159ce833b2001d78031aceb2b
Razma333 <Raz5945@gmail.com>
2017-03-23 15:38:30 +0200

    include component & html - bomb

--- src/app/bomb/bomb.component.ts
+++ src/app/bomb/bomb.component.ts
@@ -14,6 +14,26 @@ import { Observable } from 'rxjs/Rx';
                 style({
                     background: 'rgba(255, 255, 255, 0.6)'
                 })))
+        ]),
+        trigger('closeLogosBomb', [
+            state('close', style({
+                transform: 'scale(0.2)'
+            })),
+            transition('close => void', animate('0.5s linear',
+                style({
+                    transform: 'scale(0)'
+                })))
+        ]),
+        trigger('rowShow', [
+            state('active', style({
+                transform: 'translate(-50%, -50%) scale(1)'
+            })),
+            transition('void => active', animate('2s ease-in-out', keyframes([
+                style({ transform: 'translate(-50%, -50%) scale(1)', offset: 0 }),
+                style({ transform: 'translate(-50%, -50%) scale(1)', offset: 0.25 }),
+                style({ transform: 'translate(-50%, -50%) scale(1.3)', offset: 0.5 }),
+                style({ transform: 'translate(-50%, -50%) scale(1)', offset: 1 }),
+            ])))
         ])
     ]
 })
@@ -21,17 +41,35 @@ import { Observable } from 'rxjs/Rx';
 export class BombComponent implements OnInit {
 
     public show: boolean = true;
-    public active: string = 'active';
+    public bombFinish = false;
+    public activeBomb: string = 'active';
+    public activeRow: string = 'inactive';
+    public closeLogosBomb: string = 'active';
 
-    timerInit(): void {
-        let timer = Observable.timer(5000, 0);
+    // making timers
+    timerInit(timeDelay: number, tickInterval: number): Observable<number> {
+        let timer = Observable.timer(timeDelay, tickInterval);
+        return timer;
+    }
+
+    // animations handler
+    animationStarter(timeDelay: number, tickInterval: number): void {
+        let timer = this.timerInit(timeDelay, tickInterval);
         let timerSubscribe = timer.subscribe((time) => {
-            this.show = false;
-            this.active = 'null';
-            timerSubscribe.unsubscribe();
+            console.log(time, 'seconds');
+            if (time == 5) {
+                this.bombFinish = true;
+                this.closeLogosBomb = 'close';
+                this.activeRow = 'active';
+            }
+            else if (time == 10) {
+                this.show = false;
+                this.activeBomb = 'close';
+                timerSubscribe.unsubscribe();
+            }
         });
     }
     ngOnInit() {
-        this.timerInit();
+        this.animationStarter(0, 1000);
     }
 }
